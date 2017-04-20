b7c59388e5bf8c6c730d1b877a5ecae846199430
Crystal Hess <crystal@adadevelopersacademy.org>
2017-04-19 22:42:38 -0700

    adds solution

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,12 @@
 def arrayEquals(a, b)
-
+  # if the lengths aren't the same, they can't be equal
+  return false if a.length != b.length
+  # loop through each index and check that the values are equal
+  a.length.times do |i|
+    # if there's a non-match, they aren't equal
+    return false if a[i] != b[i]
+  end
+  # if looped through all without finding a mismatch,
+  # then the arrays are equal
+  return true
 end
